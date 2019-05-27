18986b7a65fdd66a89c691b9fc5618ce799a7a56
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2019-05-27 15:01:26 +0900

    操作説明の動画を追加

---
+++ Assets/Kakihana/Scripts/MovieSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class MovieSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.TimerFrame(1600).Subscribe(c =>
+        {
+            this.gameObject.SetActive(false);
+        }).AddTo(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
