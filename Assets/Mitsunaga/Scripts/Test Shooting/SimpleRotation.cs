845339a6b288830cd0f0fd45874f1b69c7c68c03
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-07-04 11:18:39 +0900

    またでデベロップで触ってしまっていた(Mitsunagaフォルダで変更したもののみ)

---
+++ Assets/Mitsunaga/Scripts/Test Shooting/SimpleRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class SimpleRotation : MonoBehaviour
+{
+    [SerializeField, Header("最大回転値")]
+    float rotSpeed = 90;
+
+
+    void Start()
+    {
+        Vector3 rot = new Vector3(
+            rotSpeed,
+            0,
+            0
+            );
+
+        Debug.Log(rot);
+
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                transform.Rotate(rot * Time.deltaTime);
+                if(transform.eulerAngles.x < -180)
+                {
+                    transform.eulerAngles += new Vector3(360, 0, 0);
+                }
+                else if (transform.eulerAngles.x > 180)
+                {
+                    transform.eulerAngles += new Vector3(-360, 0, 0);
+                }
+            })
+            .AddTo(this.gameObject);
+    }
+}
