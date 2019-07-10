5be4ee720ab210ccdc6a8457040f56e4557e85d5
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-07-10 14:17:43 +0900

    UI用のシェーダーやテクスチャの実装、フォルダ整理

---
+++ Assets/Mitsunaga/Scripts/Test Shooting/WireFrameFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireFrameFilter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Quads, 0);
+    }
+}
